4a572aad9df94c2850e70154c8d5bf76ae9d49ea
yosukearakaki <yosuke1225@gmail.com>
2017-06-05 03:40:15 +0000

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
-  }
+  devise_for :users #, controllers: {
+  #  omniauth_callbacks: "users/omniauth_callbacks"
+  #}
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
